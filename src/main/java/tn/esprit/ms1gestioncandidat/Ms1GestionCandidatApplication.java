087efaa26da78d11afe1b96a7a7feeee0ed1ff01
feres.bensassi <feresbensassi5@gmail.com>
2023-06-22 15:52:43 +0100

    add eureka properties

--- src/main/java/tn/esprit/ms1gestioncandidat/Ms1GestionCandidatApplication.java
+++ src/main/java/tn/esprit/ms1gestioncandidat/Ms1GestionCandidatApplication.java
@@ -3,11 +3,13 @@ package tn.esprit.ms1gestioncandidat;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import tn.esprit.ms1gestioncandidat.entities.Candidat;
 import tn.esprit.ms1gestioncandidat.repositories.CandidatRepository;
 import java.util.stream.Stream;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Ms1GestionCandidatApplication {
 
